2552d99922d3b75efb76ba5b25f5894ef0718cb1
Daria <daria@fitingo.com>
2022-07-19 11:49:33 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Дарья Леонова on 19.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
